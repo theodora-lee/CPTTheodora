175f234d71af2615f21bd08cabefb550b1b3f48e
theodora-lee <theodora.lee27@ycdsbk12.ca>
2025-05-24 02:21:26 -0400

    Update CPTTheodora.java

--- CPTTheodora.java
+++ CPTTheodora.java
@@ -4,16 +4,41 @@ public class CPTTheodora{
 	public static void main(String[] args){
 		Console con = new Console();
 		
+
+	}
+	
+	/*	public static int Score(int intScore){
+			TextOutputFile score = new TextOutputFile("ScoreRecord.txt",true);
+			score.println(intScore);
+			TextInputFile read = new TextInputFile("ScoreRecord.txt");
+			int i = 1;
+			int i2 = 1;
+			while(read.eof() == false){
+				i2 = read.readInt();
+				if(i < i2){
+					i2 = i;
+				}
+			}	
+			return i2;
+	}*/	
+	
+	public static int Math(int intMin, int intMax){	
+		Console con = new Console();
 		//actual gameplay
-		int intx1;
-		int intx2;
 		int intOpSign;
-		int intAns;
-		intx1 = (int)(Math.random()*10+1);
+		int intAns = 1;
+		int setup;
+		int intx1 = (int)(Math.random()*intMax+intMin);
 		//con.println(intx1);
-		intx2 = (int)(Math.random()*10+1);
+		int intx2 = (int)(Math.random()*intMax+intMin);
 		//con.println(intx2);
-		intOpSign = (int)(Math.random()*4+1);
+		//to ensure that the answer rn is only whole numbers
+		if(intx1 % intx2 == 0){
+			setup = 4;
+		}else{
+			setup = 3;
+		}		
+		intOpSign = (int)(Math.random()*setup+1);
 		switch(intOpSign){
 			case 1:
 				intAns = intx1 + intx2;
@@ -34,7 +59,7 @@ public class CPTTheodora{
 			default:
 				break;
 		}		
-				
-			
+		
+		return intAns;		
 	}
 }		
