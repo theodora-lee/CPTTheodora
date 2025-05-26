feca06ce3dd61b99f4ef08d8199a928ee611c723
theodora-lee <theodora.lee27@ycdsbk12.ca>
2025-05-26 16:01:07 -0400

    Update CPTTheodora.java

--- CPTTheodora.java
+++ CPTTheodora.java
@@ -4,61 +4,6 @@ public class CPTTheodora{
 	public static void main(String[] args){
 		Console con = new Console();
 		
-
-	}
-	
-	/*	public static int Score(int intScore){
-			TextOutputFile score = new TextOutputFile("ScoreRecord.txt",true);
-			score.println(intScore);
-			TextInputFile read = new TextInputFile("ScoreRecord.txt");
-			int i = 1;
-			int i2 = 1;
-			while(read.eof() == false){
-				i2 = read.readInt();
-				if(i < i2){
-					i2 = i;
-				}
-			}	
-			return i2;
-	}*/	
-	
-	public static int Math(int intMin, int intMax){	
-		Console con = new Console();
-		//actual gameplay
-		int intOpSign;
-		int intAns = 1;
-		int setup;
-		int intx1 = (int)(Math.random()*intMax+intMin);
-		//con.println(intx1);
-		int intx2 = (int)(Math.random()*intMax+intMin);
-		//con.println(intx2);
-		//to ensure that the answer rn is only whole numbers
-		if(intx1 % intx2 == 0){
-			setup = 4;
-		}else{
-			setup = 3;
-		}		
-		intOpSign = (int)(Math.random()*setup+1);
-		switch(intOpSign){
-			case 1:
-				intAns = intx1 + intx2;
-				con.println(intx1+ " + "+intx2);
-				break;
-			case 2:
-				intAns = intx1 - intx2;
-				con.println(intx1+ " - "+intx2);
-				break;	
-			case 3:
-				intAns = intx1 * intx2;
-				con.println(intx1+ " * "+intx2);
-				break;
-			case 4:	
-				intAns = intx1 / intx2;
-				con.println(intx1+ " / "+intx2);
-				break;
-			default:
-				break;
-		}		
 		
 	
 	}		
