cdb6ba3aed1f5132fc26942af9efebd882fbcb1e
theodora-lee <theodora.lee27@ycdsbk12.ca>
2025-05-29 10:39:54 -0400

    .

--- CPTTheodora.java
+++ CPTTheodora.java
@@ -4,7 +4,109 @@ public class CPTTheodora{
 	public static void main(String[] args){
 		Console con = new Console();
 		
+		boolean blnstartup = true;
+		boolean blnplay = false;
+		boolean blnleaderboard = false;
+		boolean blnAddQuiz = false;
 		
+
+		
+		if(blnstartup == true){
+			//load starting screen
+			if(blnplay == true){
+				con.println("name?");
+				String strName = con.readLine();
+				con.println("quiz");
+				String strQuiz = con.readLine();
+				strQuiz+=".txt";
+				con.println(strQuiz);
+				System.out.println(strQuiz+"...loading");
+				TextInputFile quiz = new TextInputFile (strQuiz);
+				int intScore = 0;
+				int intcount = 0; 
+				while(!quiz.eof()){
+					String read  = quiz.readLine();
+					intcount++;
+				}
+				intcount = intcount/4;
+				System.out.println("There are " +intcount+" questions");
+				quiz.close();
+				TextInputFile dply = new TextInputFile (strQuiz);
+				String[][] QnA = new String[intcount][5];
+				for( int i = 0; i < intcount; i ++){
+					QnA[i][0] = dply.readLine();
+					//System.out.println(QnA[i][0]);
+					QnA[i][1] = dply.readLine();
+					//System.out.println(QnA[i][1]);
+					QnA[i][2] = dply.readLine();
+					//System.out.println(QnA[i][2]);
+					QnA[i][3] = dply.readLine();
+					//System.out.println(QnA[i][3]);
+					int intRand = (int)(Math.random()*100+1);
+					QnA[i][4] = String.valueOf((intRand));
+					//System.out.println(QnA[i][4]);
+				}	
+				for(int sort = 0; sort < intcount-1; sort ++){
+					for(int Qgen = 0; Qgen < intcount-1; Qgen++){
+						if(Integer.parseInt(QnA[Qgen][4]) > Integer.parseInt(QnA[Qgen+1][4])){
+							String[] temp = QnA[Qgen];
+							QnA[Qgen] = QnA[Qgen +1];
+							QnA[Qgen +1] = temp;
+						}
+					}
+				}			
+				
+				for (int print = 0; print < intcount; print ++){
+					con.println(QnA[print][0]);
+					String userIn = con.readLine();
+					for(int ans = 1; ans < 3; ans ++){
+						//System.out.println(QnA[print][ans]);
+						if(QnA[print][ans].equalsIgnoreCase(userIn)){
+							intScore ++;
+						}	
+					}
+				}	
+				System.out.println(intScore);
+				dply.close();
+				
+				TextOutputFile score = new TextOutputFile("leaderboard.txt",true);
+				score.println(strName);
+				score.println(strQuiz);
+				score.println(intScore);
+				score.close();
+			
+			}
+			
+			//leaderbaord
+			if(blnleaderboard == true){
+				functions.LeaderboardPrint(con);	
+			}		
+			if(blnAddQuiz == true){
+				con.println("What is your quiz name?");
+				String strQuizName = con.readLine();
+				TextOutputFile write = new TextOutputFile("quizes.txt", true);
+				write.println(strQuizName);
+				strQuizName = strQuizName + ".txt";
+				write.close();
+				TextOutputFile add = new TextOutputFile(strQuizName);
+				int intAddNew = 1;
+				while (intAddNew == 1){
+					con.println("Question:");
+					String Question= con.readLine();
+					add.println(Question);
+					for(int i = 1; i <= 3; i++){
+						con.println("Answer "+i);
+						String strAnswer = con.readLine();
+						add.println(strAnswer);
+					}	
+					con.println("more?");
+					intAddNew = con.readInt();
+				}	
+				
+			}	
+			
+			blnstartup = false;
+		}	
 	
 	}		
 }		
--- functions.java
+++ functions.java
@@ -1,62 +1,43 @@
 import arc.*;
 
 public class functions{
-	public static int Equations(){
-		Console con = new Console();
-		TextOutputFile eqn = new TextOutputFile ("expressions.txt", false);
-		int intOpSign;
-		int intAns = 1;
-		int setup;
-		int intx1 = (int)(Math.random()*10+1);
-		//con.println(intx1);
-		int intx2 = (int)(Math.random()*10+1);
-		//con.println(intx2);
-		//to ensure that the answer rn is only whole numbers
-		if(intx1 % intx2 == 0){
-			setup = 4;
-		}else{
-			setup = 3;
-		}		
-		intOpSign = (int)(Math.random()*setup+1);
-		switch(intOpSign){
-			case 1:
-				intAns = intx1 + intx2;
-				eqn.println(intx1+ " + "+intx2);
-				break;
-			case 2:
-				intAns = intx1 - intx2;
-				eqn.println(intx1+ " - "+intx2);
-				break;	
-			case 3:
-				intAns = intx1 * intx2;
-				eqn.println(intx1+ " * "+intx2);
-				break;
-			case 4:	
-				intAns = intx1 / intx2;
-				eqn.println(intx1+ " / "+intx2);
-				break;
-			default:
-				break;
-			}	
-		return intAns;	
-	}	
-	public static int HighScore(int Score){
-		TextOutputFile score = new TextOutputFile("ScoreRecord.txt",true);
-		score.println(Score);
-		score.close();
-		
-		TextInputFile read = new TextInputFile("ScoreRecord.txt");
-		int i = 1;
-		int i2 = 1;
-		
-		while(read.eof() == false){
-			i2 = read.readInt();
-			//con.println(i2);
-			if(i < i2){
-				i = i2;
-			}
+
+	public static void LeaderboardPrint(Console con){
+		int intcount = 0;	
+		TextInputFile ldb = new TextInputFile("leaderboard.txt");
+		while(ldb.eof() == false){
+			String read  = ldb.readLine();
+			intcount++;
+			System.out.println("leaderboard count =" +intcount);
 		}
-		read.close();
-		return i;
+		ldb.close();
+			
+		int intLeaderBoardCount = intcount/3;
+				
+		TextInputFile lead = new TextInputFile("leaderboard.txt");
+			
+		String[][] LeaderBoard = new String[intcount/3][3];
+		
+		for(int intLBCount = 0; intLBCount < intLeaderBoardCount; intLBCount ++){
+			LeaderBoard[intLBCount][0]=lead.readLine();
+			LeaderBoard[intLBCount][1]=lead.readLine();
+			LeaderBoard[intLBCount][2]=lead.readLine();
+		}	
+			String strNameTemp;
+			String strQuizTemp;
+			String strScoreTemp;
+			for(int ii = 0; ii < intLeaderBoardCount-1; ii ++){
+				for(int i = 0; i < intLeaderBoardCount-1; i ++){
+					if(Integer.parseInt(LeaderBoard[i][2])<Integer.parseInt(LeaderBoard[i+1][2])){
+						String[] temp = new String[intLeaderBoardCount];
+						temp = LeaderBoard[i];
+						LeaderBoard[i] = LeaderBoard[i+1];
+						LeaderBoard[i+1] = temp;
+					}	
+				}
+			}
+			for(int i = 0; i <intLeaderBoardCount; i ++){
+				con.println(LeaderBoard[i][0]+"	"+LeaderBoard[i][1]+"	"+LeaderBoard[i][2]);
+			}	
 	}	
-}			
+}				
