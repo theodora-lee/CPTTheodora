d132ea532f90c6ef6734d05b58e812f5823e7718
theodora-lee <theodora.lee27@ycdsbk12.ca>
2025-06-10 09:40:45 -0400

    trial on the confetti in tryouts

--- tryout.java
+++ tryout.java
@@ -7,9 +7,43 @@ public class tryout{
 	public static void main(String[] args){
 		Console con = new Console();
 		
-		while(true){
-			int a = con.getKey();
-			con.println(a);
-		}	
+ int confettiCount = 50;
+        int[] x = new int[confettiCount];
+        int[] y = new int[confettiCount];
+        Color[] colors = new Color[confettiCount];
+        boolean blnConfetti = true;
+
+        // Initialize confetti with random positions and colors
+        for (int i = 0; i < confettiCount; i++) {
+            x[i] = (int)(Math.random() * 1000);
+            y[i] = (int)(Math.random() * -720); // Start above screen
+            colors[i] = new Color(
+                (int)(Math.random() * 256),
+                (int)(Math.random() * 256),
+                (int)(Math.random() * 256)
+            );
+        }
+
+        while (blnConfetti) {
+            con.setBackgroundColor(Color.BLACK);
+            con.clear();
+
+            for (int i = 0; i < confettiCount; i++) {
+                con.setDrawColor(colors[i]);
+                con.fillRect(x[i], y[i], 5, 5); // Confetti size 5x5
+                y[i] += (int)(Math.random()*2); // Fall speed
+
+                // Reset when it reaches bottom
+                if (y[i] > 720) {
+                    x[i] = (int)(Math.random() * 1000);
+                    y[i] = (int)(Math.random() * -50);
+                    colors[i] = new Color(
+                        (int)(Math.random() * 256),
+                        (int)(Math.random() * 256),
+                        (int)(Math.random() * 256)
+                    );
+                }
+            }
+		}
 	}
 }		
